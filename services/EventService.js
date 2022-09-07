b8dcfcb4a5674f5cdfdabbd453bab36b044c86ec
rcastillod <rodrigo.castillo@global3.cl>
2022-09-07 17:17:28 -0400

    feat: Web API

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://my-json-server.typicode.com/Code-Pop/real-world-nuxt',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
@@ -14,6 +14,6 @@ export default {
     return apiClient.get('/events')
   },
   getEvent(id) {
-    return apiClient.get('events/'+id)
+    return apiClient.get('events/' + id)
   }
 }
