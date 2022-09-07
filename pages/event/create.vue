b91a0585314c932d0e274589a9513137bb89a20a
rcastillod <rodrigo.castillo@global3.cl>
2022-09-07 16:50:59 -0400

    feat: Vuex implemented

---
+++ pages/event/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Create and Event</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'create-page',
+    head() {
+      return {
+        title: 'Create an Event',
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: 'Create an event in your neighborhood'
+          }
+        ]
+      }
+    }
+  }
+</script>
